98ce847e11e06fa23de7e0e7640a0bb5bcc4124b
xavi447 <alexandersoriano1022@gmail.com>
2025-02-03 18:01:22 -0600

    Tarea: Calculadora con factorial, porcentaje, exponenciacion, raiz y modulo

--- app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
@@ -55,6 +55,46 @@ public class MainActivity extends AppCompatActivity {
                         respuesta = num1 / num2;
                         msg = "La division es: "+ respuesta;
                         break;
+                    case 4:
+                        //Factor
+                        if (num1 >= 0) {
+                            int fact = 1;
+                            for (int i = 1; i <= num1; i++) {
+                                fact *= i;
+                            }
+                            msg = "El factorial de " + num1 + " es: " + fact;
+                        }  else {
+                            msg = "Error: No se puede calcular el factorial de un número negativo.";
+                        }
+                        break;
+                    case 5:
+                        // Porcentaje
+                        respuesta = (num1 / 100.0) * num2;
+                        msg = num1 + "% de " + num2 + " es: " + respuesta;
+                        break;
+                    case 6:
+                        // Exponenciación
+                        respuesta = Math.pow(num1, num2);
+                        msg = num1 + " elevado a " + num2 + " es: " + respuesta;
+                        break;
+                    case 7:
+                        // Raíz
+                        if (num2 != 0) {
+                            respuesta = Math.pow(num1, 1.0 / num2);
+                            msg = "La raíz de índice " + num2 + " de " + num1 + " es: " + respuesta;
+                        } else {
+                            msg = "Error: El índice de la raíz no puede ser cero.";
+                        }
+                        break;
+                    case 8:
+                        // Módulo
+                        if (num2 != 0) {
+                            respuesta = num1 % num2;
+                            msg = "El módulo de " + num1 + " y " + num2 + " es: " + respuesta;
+                        } else {
+                            msg = "Error: No se puede calcular el módulo con divisor cero.";
+                        }
+                        break;
                 }
                 tempVal = findViewById(R.id.lblRespuesta);
                 tempVal.setText("Respuesta: "+ respuesta);
