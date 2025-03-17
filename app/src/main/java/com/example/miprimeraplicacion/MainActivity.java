64dcdb243d0f0e1609306cac2d0bb58bcd6d685f
xavi447 <alexandersoriano1022@gmail.com>
2025-03-17 09:53:51 -0600

    Creacion de rama tiendaProductoSQL

--- app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraplicacion/MainActivity.java
@@ -2,173 +2,21 @@ package com.example.miprimeraplicacion;
 
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Intent;
-import android.text.Editable;
-import android.text.TextWatcher;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import org.json.JSONObject;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.widget.ImageView;
-import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
-    Button btn;
-    TextView tempVal;
-    DB db;
-    String accion = "nuevo", idAmigo = "";
-    ImageView img;
-    String urlCompletaFoto = "";
-    Intent tomarFotoIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img = findViewById(R.id.imgFotoAmigo);
-        db = new DB(this);
-        btn = findViewById(R.id.btnGuardarAmigo);
-        btn.setOnClickListener(view -> guardarAmigo());
 
-        fab = findViewById(R.id.fabListaAmigos);
-        fab.setOnClickListener(view -> abrirVentana());
-        mostrarDatos();
-        tomarFoto();
 
     }
+}
 
-
-    private void mostrarDatos() {
-        try {
-            Bundle parametros = getIntent().getExtras();
-            accion = parametros.getString("accion");
-            if (accion.equals("modificar")) {
-                JSONObject datos = new JSONObject(parametros.getString("amigos"));
-                idAmigo = datos.getString("idAmigo");
-
-                tempVal = findViewById(R.id.txtNombre);
-                tempVal.setText(datos.getString("nombre"));
-
-                tempVal = findViewById(R.id.txtDireccion);
-                tempVal.setText(datos.getString("direccion"));
-
-                tempVal = findViewById(R.id.txtTelefono);
-                tempVal.setText(datos.getString("telefono"));
-
-                tempVal = findViewById(R.id.txtEmail);
-                tempVal.setText(datos.getString("email"));
-
-                tempVal = findViewById(R.id.txtDui);
-                tempVal.setText(datos.getString("dui"));
-
-                urlCompletaFoto = datos.getString("urlFoto");
-                img.setImageURI(Uri.parse(urlCompletaFoto));
-            }
-        }catch (Exception e){
-            mostrarMsg("Error: "+e.getMessage());
-        }
-    }
-    private void tomarFoto(){
-        img.setOnClickListener(view->{
-            tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            File fotoAmigo = null;
-            try{
-                fotoAmigo = crearImagenAmigo();
-                if( fotoAmigo!=null ){
-                    Uri uriFotoAimgo = FileProvider.getUriForFile(MainActivity.this,
-                            "com.example.miprimeraplicacion.fileprovider", fotoAmigo);
-                    tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFotoAimgo);
-                    startActivityForResult(tomarFotoIntent, 1);
-                }else{
-                    mostrarMsg("Nose pudo crear la imagen.");
-                }
-            }catch (Exception e){
-                mostrarMsg("Error: "+e.getMessage());
-            }
-        });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try{
-            if( requestCode==1 && resultCode==RESULT_OK ){
-                //Bitmap imagenBitmap = BitmapFactory.decodeFile(urlCompletaFoto);
-                img.setImageURI(Uri.parse(urlCompletaFoto));
-            }else{
-                mostrarMsg("No se tomo la foto.");
-            }
-        } catch (Exception e) {
-            mostrarMsg("Error: " + e.getMessage());
-        }
-    }
-    private File crearImagenAmigo() throws Exception{
-        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
-                fileName = "imagen_"+ fechaHoraMs+"_";
-        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
-        if( dirAlmacenamiento.exists()==false ){
-            dirAlmacenamiento.mkdir();
-        }
-        File image = File.createTempFile(fileName, ".jpg", dirAlmacenamiento);
-        urlCompletaFoto = image.getAbsolutePath();
-        return image;
-    }
-
-    private void mostrarMsg(String msg) {
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-    }
-        private void abrirVentana () {
-            Intent intent = new Intent(this, lista_amigos.class);
-            startActivity(intent);
-        }
-        private void guardarAmigo () {
-            tempVal = findViewById(R.id.txtNombre);
-            String nombre = tempVal.getText().toString();
-
-            tempVal = findViewById(R.id.txtDireccion);
-            String direccion = tempVal.getText().toString();
-
-            tempVal = findViewById(R.id.txtTelefono);
-            String telefono = tempVal.getText().toString();
-            tempVal = findViewById(R.id.txtEmail);
-            String email = tempVal.getText().toString();
-
-            tempVal = findViewById(R.id.txtDui);
-            String dui = tempVal.getText().toString();
-
-            String[] datos = {idAmigo, nombre, direccion, telefono, email, dui, urlCompletaFoto};
-            db.administrar_amigos(accion, datos);
-            Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
-            abrirVentana();
-
-
-        }
-    }
 
 
 
